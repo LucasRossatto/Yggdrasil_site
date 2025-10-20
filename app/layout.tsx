5c221f3d8c821ba5235dec456264d50c5e645202
LucasRossatto <lucas.sant.anagn3@gmail.com>
2025-10-20 08:50:20 -0300

    analytics ajustado

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,35 +30,33 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      <Analytics />
-      <html
-        lang="pt-BR"
-        className={`${poppins.variable} ${montserrat.variable}`}
-        suppressHydrationWarning
-      >
-        <head>
-          <link
-            rel="icon"
-            href="/images/yggdrasil-logo.png"
-            type="image/png"
-            sizes="any"
-          />
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css"
-          ></link>
-        </head>
-        <body>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            disableTransitionOnChange={false}
-          >
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    </>
+    <html
+      lang="pt-BR"
+      className={`${poppins.variable} ${montserrat.variable}`}
+      suppressHydrationWarning
+    >
+      <head>
+        <link
+          rel="icon"
+          href="/images/yggdrasil-logo.png"
+          type="image/png"
+          sizes="any"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css"
+        ></link>
+      </head>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          disableTransitionOnChange={false}
+        >
+          {children}
+          <Analytics />
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
