fe3e0208d0f92e48190c5de6524a8f2e856d9efc
LucasRossatto <lucas.sant.anagn3@gmail.com>
2025-10-20 08:41:26 -0300

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react";
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Poppins, Montserrat } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const poppins = Poppins({
@@ -29,32 +30,35 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="pt-BR"
-      className={`${poppins.variable} ${montserrat.variable}`}
-      suppressHydrationWarning
-    >
-      <head>
-        <link
-          rel="icon"
-          href="/images/yggdrasil-logo.png"
-          type="image/png"
-          sizes="any"
-        />
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css"
-        ></link>
-      </head>
-      <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          disableTransitionOnChange={false}
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <>
+      <Analytics />
+      <html
+        lang="pt-BR"
+        className={`${poppins.variable} ${montserrat.variable}`}
+        suppressHydrationWarning
+      >
+        <head>
+          <link
+            rel="icon"
+            href="/images/yggdrasil-logo.png"
+            type="image/png"
+            sizes="any"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css"
+          ></link>
+        </head>
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            disableTransitionOnChange={false}
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
   );
 }
